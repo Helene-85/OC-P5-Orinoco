b38829640f2432574298f4a9b981d7944c8c0be4
Helene-mb <bunel.h85@outlook.fr>
2020-09-01 09:38:47 +0200

    Ajout d'une desscription alt à l'image du produit

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,17 +2,27 @@ var request = new XMLHttpRequest();
 
 request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var products = JSON.parse(this.responseText);
-
-        products.forEach((liste) => {
-            displayProducts(liste);
-        });
+        let products = JSON.parse(this.responseText);
+        
+        products.forEach((meuble) => {
+            displayProduct(meuble);
+          });
     }
 };
 
 request.open("GET", "http://localhost:3000/api/furniture");
 request.send();
 
-function displayProducts(liste) {
-    document.getElementById('productlist').innerHTML += renderProducts(liste);
+function displayProduct(meuble) {
+  document.getElementById('productList').innerHTML += `
+  <div class="card">
+    <h2 class="card-name">${meuble.name}<h2>
+    <img class="card-img" src="${meuble.imageURL} alt="meuble en chêne"/>
+    <p class="card-id">${meuble._id}</p>
+    <p class="card-varnish">${meuble.varnish}<p>
+    <p class="card-price">${meuble.price / 100},00€</p>
+    <p class="card-description>${meuble.description}</p>
+    <a href="pages/product.html" class="card-btn">Description</a>
+  </div>
+    `
 }
