2489470acbfc6efffceb7647b57dc42b335fb739
Helene-mb <bunel.h85@outlook.fr>
2020-09-07 16:22:07 +0200

    Push de routine

--- frontend/js/product.js
+++ frontend/js/product.js
@@ -1,5 +1,14 @@
-ajax("http://localhost:3000/api/furniture");
+let urlParams = new URLSearchParams(window.location.search);
+console.log(urlParams);
+let id = urlParams.get("id");
+console.log(id);
 
-function displayProduct(products) {
+ajax("http://localhost:3000/api/furniture/" + id).then((meuble) => {
+    displayProduct(meuble);
+    console.log(displayProduct);
+})
+
+
+function displayProduct(meuble) {
     document.getElementById('productSingle').innerHTML += renderProduct(meuble, "Single");
 };
