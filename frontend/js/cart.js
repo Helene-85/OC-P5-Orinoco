8a71025f9bb9e1005af31cc70c04cc5134d0af3f
Helene-mb <bunel.h85@outlook.fr>
2020-09-17 11:45:04 +0200

    Création process de contrôle des inputs utlisateurs dans le formulaire

--- frontend/js/cart.js
+++ frontend/js/cart.js
@@ -1,20 +1,25 @@
-function getProductFromLocalStorage() {
-    let productInCart = localStorage.getItem('product');
-    if (productInCart) {
-        ajax("http://localhost:3000/api/furniture").then((products) => {
-        products.forEach((products) => {
-        displayProduct(products);
-        });
-    });
-    } else {
-        return `
-        <h3>Votre panier est vide</h3>
-        `
-    }
+let items =  JSON.parse(localStorage.getItem('products'));
+console.log(items);
+
+let res = items.find(showItem)
+
+console.log(res);
+
+function showItem(item) {
+    /* for (let i = 0; i < res.length; i++) {
+        if (res == i) {
+            ajax("http://localhost:3000/api/furniture").then((products) => {
+            products.forEach((products) => {
+            displayProduct(products);
+            });
+        })
+    }} */
+    return item === "5beaadda1c9d440000a57d98";
 }
 
-getProductFromLocalStorage();
 
 function displayProduct(products) {
-document.getElementById('productCart').innerHTML += renderProduct(products, "Cart");
-};
+    document.getElementById('productCart').innerHTML += renderProduct(products, "Cart");
+    };
+
+
