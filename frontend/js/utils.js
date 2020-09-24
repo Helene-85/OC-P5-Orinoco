a4576d6dc966f01368185f5ad4db6218dd8bf0a6
Helene-mb <bunel.h85@outlook.fr>
2020-09-24 14:54:42 +0200

    Designe du bouton de suppresion de produit avec Sass

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -84,7 +84,7 @@ function renderProduct(meuble, type) {
         <div class="cart__right-side">
           <h3 class="cart__single-name">${meuble.name}<h3>
           <p class="cart__single-price">${meuble.price / 100},00€</p>
-          <button id="remove-${meuble._id}"><i class="fas fa-trash"></i></button>
+          <button class="suppr" id="remove-${meuble._id}"><i class="fas fa-trash"></i></button>
         </div>
       </div>
         `
